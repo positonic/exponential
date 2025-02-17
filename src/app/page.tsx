da6a47978f761d7a00c3439f07e5679364411c17
J△MΞS <jamespfarrell@gmail.com>
2025-02-17 01:29:28 +0100

    feat: Add Suspense for code splitting in Home, Projects pages
    - Introduce Suspense for lazy loading in Home and Projects pages
    - Create async functions for rendering content based on user session
    - Enhance user experience by optimizing component loading times

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,22 @@ import Chat from "~/app/_components/Chat";
 import { auth } from "~/server/auth";
 import { HydrateClient } from "~/trpc/server";
 import { LandingPage } from "~/app/_components/LandingPage";
+import { Suspense } from "react";
 
 export default async function Home() {
-  const session = await auth();
-
   return (
     <HydrateClient>
-        <main className="min-h-screen w-full bg-gradient-to-b from-[#111111] to-[#212121] text-white">       
-          {session?.user ? <Chat /> : <LandingPage />}
-        </main>
+      <main className="min-h-screen w-full bg-gradient-to-b from-[#111111] to-[#212121] text-white">       
+        <Suspense fallback={<div>Loading...</div>}>
+          <HomeContent />
+        </Suspense>
+      </main>
     </HydrateClient>
   );
+}
+
+async function HomeContent() {
+  const session = await auth();
+  return session?.user ? <Chat /> : <LandingPage />;
 }
 
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 import { Projects } from "~/app/_components/Projects";
 import { Welcome } from "~/app/_components/Welcome";
+import { Suspense } from "react";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -15,9 +16,16 @@ export default async function Home() {
     <HydrateClient>
       <main className="flex h-full flex-col items-center justify-center bg-gradient-to-b from-[#111111] to-[#212121] text-white">       
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          {session?.user ? <Projects /> : <Welcome />}
+          <Suspense fallback={<div>Loading...</div>}>
+            <ProjectsWrapper />
+          </Suspense>
         </div>
       </main>
     </HydrateClient>
   );
+}
+
+async function ProjectsWrapper() {
+  const session = await auth();
+  return session?.user ? <Projects /> : <Welcome />;
 }
