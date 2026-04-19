367bfd37ae6701732b7315f8b21bcb66910bdfa3
positonic <jamespfarrell@gmail.com>
2026-04-19 22:38:56 +0200

    feat: fix ai agent slideout not workgin

--- src/app/(sidemenu)/layout.tsx
+++ src/app/(sidemenu)/layout.tsx
@@ -22,6 +22,7 @@ import { getThemeDomain } from '~/config/site';
 import { Analytics } from '@vercel/analytics/next';
 import { GoogleAnalytics } from '@next/third-parties/google';
 import { FloatingChatButton } from '~/app/_components/FloatingChatButton';
+import { AgentChatModal } from '~/app/_components/layout/AgentChatModal';
 import { ColorSchemeScript } from '~/app/_components/layout/ColorSchemeScript';
 import { MantineRootProvider } from '~/app/_components/layout/MantineRootProvider';
 import { ColorSchemeProvider } from '~/app/_components/layout/ColorSchemeProvider';
@@ -76,6 +77,7 @@ export default async function RootLayout({
                         </Layout>
                         <ServiceWorkerRegistration />
                         <FloatingChatButton />
+                        <AgentChatModal />
                       </WorkspaceProvider>
                     </ColorSchemeProvider>
                   </BugReportProvider>
